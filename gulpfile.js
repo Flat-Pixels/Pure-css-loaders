b91fd6d3b7325aac6ce6d7a2908ec88f7e986d4a
Flat-Pixels <steeveverdieu@gmail.com>
2018-10-29 15:25:14 -0400

    chore(gulp): update gulp for build
    Update gulp to give it the power to make a build version of the code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,52 @@
 var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
 
 sass.compiler = require( 'node-sass' );
 
-//Task that compiles the node-sass
+/*
+* Task that compiles the node-sass
+*/
 gulp.task( 'sass', function() {
   return gulp.src( './sass/**/*.scss' )
     .pipe( sass.sync( { outputStyle: 'expanded' } ).on( 'error', sass.logError ) )
     .pipe( gulp.dest( './css' ) );
 });
 
-//Task watching the default tasks
+/*
+* Task watching the files
+*/
 gulp.task( 'sass:watch', [ 'sass' ], function() {
   gulp.watch( './sass/**/*.scss', [ 'sass' ])
-})
+});
+
+/*
+* Task for auto-prefix the css
+*/
+gulp.task( 'autoprefixer', function() {
+  return gulp.src( './css/**/*.css' )
+    .pipe( autoprefixer( { flexbox: true, cascade: false, browsers: [ 'last 2 versions' ] } ) )
+    .pipe( gulp.dest( './css' ) );
+});
+
+/*
+* Task build the old build production version
+*/
+gulp.task( 'clean-build', function() {
+  return gulp.src( './build', {read: false})
+        .pipe(clean());
+});
+
+/*
+* Task build the production version
+*/
+gulp.task( 'build', [ 'clean-build', 'autoprefixer' ], function() {
+  return gulp.src( [ './css/**/*.css', './index.html' ] )
+        .pipe( gulp.dest( './build' ) );
+});
 
 
 gulp.task( 'default', [ 'sass:watch' ], function() {
-})
+  //Do nothing here
+});
